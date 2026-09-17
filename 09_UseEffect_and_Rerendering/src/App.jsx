8fda1227a55f5660aa537fb1411181e99941b35a
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-17 22:30:51 +0530

    Add data fetching function to App component using axios

--- 09_UseEffect_and_Rerendering/src/App.jsx
+++ 09_UseEffect_and_Rerendering/src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import About from './Components/About'
 import Contact from './Components/Contact'
+import axios from "axios"
 
 const App = () => {
   const [count, setCount] = useState(0)
   const [toggle, setToggle] = useState(false)
+
+  let getData = async() =>{
+      let res = await axios.get("https://fakestoreapi.com/products")
+  console.log(res)
+  }
+
+  getData()
 
   // useEffect(() => {
   //   console.log('useEffect called')
