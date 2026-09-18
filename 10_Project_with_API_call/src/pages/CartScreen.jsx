2b2702bd77b6ac4e04beaa165ccb82f6831d7942
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-18 10:58:22 +0530

    feat: add CartScreen component for displaying cart section

---
+++ 10_Project_with_API_call/src/pages/CartScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartScreen = () => {
+  return (
+    <div className='h-screen text-6xl'>
+      <h1>this is cart section</h1>
+    </div>
+  )
+}
+
+export default CartScreen
