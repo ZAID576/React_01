bfaa5c08cfdad5d0b24fc9cee9059851603a28d0
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-18 10:57:27 +0530

    feat: update Navbar component to handle cart state with onClick events

--- 10_Project_with_API_call/src/Components/Navbar.jsx
+++ 10_Project_with_API_call/src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({setisCartOpen}) => {
   return (
     <div className='h bg-amber-200 rounded p-5 flex items-center justify-between'>
       <div>Logo</div>
       <div className='flex gap-4'>
-        <p>Home</p>
-        <p>Cart</p>
+        <p onClick={()=>setisCartOpen(false)}>Home</p>
+        <p onClick={()=>setisCartOpen(true)}>Cart</p>
       </div>
       <button>Login</button>
     </div>
