8240e84c34194c6692a48278228ff31b4ca6ebca
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-19 19:39:16 +0530

    useNavigate()

--- 12_Nested_Routing/src/pages/About.jsx
+++ 12_Nested_Routing/src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 import { Outlet } from 'react-router'
 
 const About = () => {
+ let navigate = useNavigate();
+
   return (
     <div>
-      This is about
+      <h1>This is about</h1>
+      <button onClick={()=>navigate("/about/nested")}>Show me nested</button>
       <Outlet />
     </div>
   )
