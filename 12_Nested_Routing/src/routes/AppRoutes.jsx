0cba51c161251edf456e7ac82a6ee016ed0a5389
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-19 19:13:02 +0530

    feat: add nested routing for NestedAbout component in AppRoutes

--- 12_Nested_Routing/src/routes/AppRoutes.jsx
+++ 12_Nested_Routing/src/routes/AppRoutes.jsx
@@ -4,17 +4,24 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import Contact from '../pages/contact';
 import NestedHome from '../pages/NestedHome';
+import NestedAbout from '../pages/NestedAbout';
 
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
+        
         <Route path="/" element={<Home />}>
          <Route path="nested-home" element={<NestedHome />}/>
         </Route>
-        <Route path="/about" element={<About />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="nested-about" element={<NestedAbout />}/>
+        </Route>
+
         <Route path="/contact" element={<Contact />} />
+      
       </Routes>
     </div>
   )
