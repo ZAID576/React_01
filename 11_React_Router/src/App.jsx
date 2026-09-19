ba1fca0dd9c47ca401fa3ab4faa6f2d0e7140fa4
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-19 12:53:40 +0530

    feat: replace inline navigation with Navbar component

--- 11_React_Router/src/App.jsx
+++ 11_React_Router/src/App.jsx
@@ -1,22 +1,12 @@
 import { NavLink} from "react-router";
 import AppRoutes from './routes/AppRoutes';
+import Navbar from "./Components/Navbar";
 
 const App = () => {
 
   return (
     <div className="h-screen p-2">
-      <nav className="flex items-center justify-between mb-4 bg-yellow-200 h-10 p-3">
-        <h1>Logo</h1>
-
-        <div className="flex items-center gap-6 justify-between">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
-
-        <button>Login</button>
-      </nav>
-
+      <Navbar />
       <AppRoutes />
 
     </div>
