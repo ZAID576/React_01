fe657af1b66e10208a94f01e24f850824925c3d0
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-20 09:36:47 +0530

    Navbar.jsx

---
+++ 13_Dynamic_Routing/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between mb-4 bg-yellow-200 h-10 p-3">
+        <h1>Logo</h1>
+
+        <div className="flex items-center gap-6 justify-between">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>about</NavLink>
+          <NavLink to={"/products"}>products</NavLink>
+        </div>
+
+        <button>Login</button>
+      </nav>
+  )
+}
+
+export default Navbar
