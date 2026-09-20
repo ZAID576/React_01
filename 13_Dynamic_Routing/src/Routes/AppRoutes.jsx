b5a69135e7c838546f6772ee6d13172718af13dd
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-20 09:37:53 +0530

    AppRoutes.jsx

---
+++ 13_Dynamic_Routing/src/Routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Products from '../pages/Products';
+
+
+const AppRoutes = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
